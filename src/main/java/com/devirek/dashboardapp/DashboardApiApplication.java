599e2fd8c2c9c141d579c73bc8abe7f059005f57
jaDevirek <wiciorny@gmail.com>
2020-06-30 23:26:24 +0200

    initial commit, creating dependencies from Spring Initializer

---
+++ src/main/java/com/devirek/dashboardapp/DashboardApiApplication.java
@@ -0,0 +1,13 @@
+package com.devirek.dashboardapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DashboardApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devirek/dashboardapp/DashboardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devirek.dashboardapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
