c9af6e6e0e21cd28187fcd03353dca8c3229bda3
Wiktor Religo <wiciorny@gmail.com>
2020-07-05 16:33:10 +0200

    Initial commit, setup first configuration class for security

---
+++ api-core/src/main/java/com/devirek/dashboardapp/security/SecurityConfig.java
@@ -0,0 +1,13 @@
+package src.main.java.com.devirek.dashboardapp.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+}
