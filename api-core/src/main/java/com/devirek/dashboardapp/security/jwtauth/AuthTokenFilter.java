0d449d4d5c9a2a1c9c634486ae887cda4a2c09dd
Wiktor Religo <wiciorny@gmail.com>
2020-07-14 22:39:00 +0200

    finishing tokenfilter class

--- api-core/src/main/java/com/devirek/dashboardapp/security/jwtauth/AuthTokenFilter.java
+++ api-core/src/main/java/com/devirek/dashboardapp/security/jwtauth/AuthTokenFilter.java
@@ -4,9 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 import src.main.java.com.devirek.dashboardapp.security.service.UserDetailsServiceImpl;
 import src.main.java.com.devirek.dashboardapp.utility.JWTUtils;
@@ -47,31 +49,41 @@ public class AuthTokenFilter extends OncePerRequestFilter {
                                     FilterChain filterChain) throws ServletException, IOException {
         try {
 
-            String jWebToken = parseJwt(request);
+            String jWebToken = parseJwt(request).get();
+
             if (Optional.ofNullable(jWebToken)
                         .isPresent() && jwtUtils.validateJwtToken(jWebToken)) {
                 UserDetails userDetails = userDetailsService.loadUserByUsername(jwtUtils.getUserNameFromJWT(jWebToken));
                 setAuthenticationByUserDetails(request, userDetails);
             }
-            filterChain.doFilter(request, response);
 
         } catch (Exception e) {
             logger.error("Cannot set user authentication: {}", e);
         }
+        filterChain.doFilter(request, response);
     }
 
     private void setAuthenticationByUserDetails(HttpServletRequest request, UserDetails userDetails) {
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails,
                                                                                                           null,
                                                                                                           userDetails.getAuthorities());
-
         authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext()
+                             .setAuthentication(authenticationToken);
     }
 
-    private String parseJwt(HttpServletRequest request) {
+    /**
+     * Bearer - prefix determines the type of authentication
+     *
+     * @return
+     */
+    private Optional<String> parseJwt(HttpServletRequest request) {
 
-
+        String authorizationHeader = request.getHeader("Authorization");
+        if (StringUtils.hasText(authorizationHeader) && authorizationHeader.startsWith("Bearer")) {
+            return Optional.of(authorizationHeader.substring(7, authorizationHeader.length()));
+        }
+        ;
+        return Optional.empty();
     }
-
-
 }
