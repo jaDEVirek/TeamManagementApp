fdb33b9d79b1327e1fc9e868f2cc8ba20793f468
Wiktor Religo <wiciorny@gmail.com>
2020-07-17 19:56:59 +0200

    end of implementation for SecurityConfig class

--- api-core/src/main/java/com/devirek/dashboardapp/security/SecurityConfig.java
+++ api-core/src/main/java/com/devirek/dashboardapp/security/SecurityConfig.java
@@ -4,11 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import src.main.java.com.devirek.dashboardapp.security.jwtauth.AuthTokenFilter;
+import src.main.java.com.devirek.dashboardapp.security.jwtauth.JwtAuthEntryPoint;
 import src.main.java.com.devirek.dashboardapp.security.service.UserDetailsServiceImpl;
 
 @Configuration
@@ -16,17 +20,20 @@ import src.main.java.com.devirek.dashboardapp.security.service.UserDetailsServic
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
+    private JwtAuthEntryPoint unauthorizedHandler;
+
     private UserDetailsServiceImpl userDetailsService;
 
     @Autowired
-    public SecurityConfig(UserDetailsServiceImpl userDetailsService) {
+    public SecurityConfig(UserDetailsServiceImpl userDetailsService, JwtAuthEntryPoint unauthorizedHandler) {
+        this.unauthorizedHandler = unauthorizedHandler;
         this.userDetailsService = userDetailsService;
     }
 
     @Override
     public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
         authenticationManagerBuilder.userDetailsService(userDetailsService)
-                                    .passwordEncoder(passwordEncoder());
+                .passwordEncoder(passwordEncoder());
     }
 
     @Bean
@@ -36,5 +43,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     public AuthTokenFilter authenticationJwtTokenFilter() {
         return new AuthTokenFilter();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors()
+                .and()
+                .csrf()
+                .disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(unauthorizedHandler)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/auth/**")
+                .permitAll()
+                .antMatchers("/auth/test/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
