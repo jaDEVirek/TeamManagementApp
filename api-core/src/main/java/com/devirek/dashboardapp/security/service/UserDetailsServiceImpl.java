a9765d0bef88a745e6b397caa7284694564e6d6f
Wiktor Religo <wiciorny@gmail.com>
2020-07-19 19:45:46 +0200

    end of implementation for service authorization package

--- api-core/src/main/java/com/devirek/dashboardapp/security/service/UserDetailsServiceImpl.java
+++ api-core/src/main/java/com/devirek/dashboardapp/security/service/UserDetailsServiceImpl.java
@@ -1,16 +1,27 @@
 package src.main.java.com.devirek.dashboardapp.security.service;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import src.main.java.com.devirek.dashboardapp.security.repository.IUserRepository;
 
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private IUserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(IUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        return UserDetailsImpl.build(userRepository.findByUsername(userName)
+                .orElseThrow(() -> new UsernameNotFoundException("User with name: " + userName + " not found!")));
     }
 }
