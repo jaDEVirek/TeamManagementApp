021b0f459b6d4db4b204e894bfbd749cbf902e6b
Wiktor Religo <wiciorny@gmail.com>
2020-07-22 19:58:06 +0200

    adding SecurityConfig annotation for Authentication testing controller, implement AuthTestController class

--- api-core/src/main/java/com/devirek/dashboardapp/security/controller/AuthTestController.java
+++ api-core/src/main/java/com/devirek/dashboardapp/security/controller/AuthTestController.java
@@ -1,7 +1,9 @@
 package src.main.java.com.devirek.dashboardapp.security.controller;
 
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +13,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth/test")
 public class AuthTestController {
 
+    @GetMapping("/all")
+    public String allAccess() {
+        return "Public Content.";
+    }
 
+    @GetMapping("/user")
+    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+    public String userAccess() {
+        return "User Content.";
+    }
 
+    @GetMapping("/mod")
+    @PreAuthorize("hasRole('MODERATOR')")
+    public String moderatorAccess() {
+        return "Moderator Board.";
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String adminAccess() {
+        return "Admin Board.";
+    }
 }
